8bafcf4a2a9205c5f3bf69a63be2d0d9fd1c9bac
vivify <alekcuric7@gmail.com>
2022-12-02 21:57:26 +0100

    added faker to Locators

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,31 +1,30 @@
 /// <reference types="Cypress" />
 
+import { faker } from '@faker-js/faker';
 const Locators = require("../fixtures/{} Locators.json");
 
 
 describe("registration test", () => {
-    function makeId(length) {
-        var result = "";
-        var characters =
-          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-          );
-        }
-        return result;
-      }
-      let email = `${makeId(5)}@test.com`;
+  let randomUser = {
+    randomEmail: faker.internet.email(),
+    randomFirstName: faker.name.firstName(),
+    randomLastName: faker.name.lastName(),
+    randomPassword: faker.internet.password()
+  }
+
+    before("visit gallery app", () => {
+      cy.visit("/");
+      cy.get("a[href='/register']").click();
+    });
 
     it("register with valid data", () => {
         cy.visit("/")
         cy.get(Locators.Register.registerButton).click();
-        cy.get(Locators.Register.firstNameInputField).type("Aleksandar");
-        cy.get(Locators.Register.lastNameInputField).type("Curic");
-        cy.get(Locators.SharedElements.emailInput).type(email);
-        cy.get(Locators.SharedElements.passwordInput).type("Qwerty123!");
-        cy.get(Locators.Register.confirmedPasswordInputField).type("Qwerty123!");
+        cy.get(Locators.Register.firstNameInputField).type(randomUser.randomFirstName);
+        cy.get(Locators.Register.lastNameInputField).type(randomUser.randomLastName);
+        cy.get(Locators.SharedElements.emailInput).type(randomUser.randomEmail);
+        cy.get(Locators.SharedElements.passwordInput).type(randomUser.randomPassword);
+        cy.get(Locators.Register.confirmedPasswordInputField).type(randomUser.randomPassword);
         cy.get(Locators.Register.checkbox).check();
         cy.get(Locators.SharedElements.submitButton).click();
 
